6fbc53d06600308033158ad67eab0c764c60a4da
Xiro SAS <proyectos@xiro.net.co>
2018-02-04 08:10:15 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/config', 'HomeController@config');
+Route::get('/config', 'HomeController@config')->name('config');
